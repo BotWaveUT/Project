7281a3e3378cc923b4b28a326c53f9ad2c671849
van3ll0pe <van3ll0pe.chell@gmail.com>
2026-02-11 16:19:14 +0100

    REM build folder

--- build/dma_c.d
+++
@@ -1,2 +0,0 @@
-build/dma_c.o: src/dma.c include/dma.h include/peripherals/base.h \
- include/i2s.h include/peripherals/gpio.h include/peripherals/base.h
--- build/input_c.d
+++
@@ -1,2 +0,0 @@
-build/input_c.o: src/input.c include/peripherals/gpio.h \
- include/peripherals/base.h include/utils.h
