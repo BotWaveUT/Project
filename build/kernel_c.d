2a67ee25426f64725b3dd5bd2c7be3bf9e888807
tinyaitmess <tinhinane.ait-messaoud@univ-tlse3.fr>
2026-02-17 19:06:02 +0100

    Keyboard working

--- build/kernel_c.d
+++
@@ -1,4 +0,0 @@
-build/kernel_c.o: src/kernel.c include/dma.h include/peripherals/base.h \
- include/i2s.h include/peripherals/gpio.h include/peripherals/base.h \
- include/input.h include/irq.h include/mini_uart.h include/oscillator.h \
- include/printf.h include/timer.h include/utils.h
--- build/oscillator_c.d
+++
@@ -1,4 +0,0 @@
-build/oscillator_c.o: src/oscillator.c include/oscillator.h include/dma.h \
- include/peripherals/base.h include/i2s.h include/peripherals/gpio.h \
- include/peripherals/base.h include/input.h include/irq.h include/math.h \
- include/mini_uart.h include/timer.h include/utils.h
